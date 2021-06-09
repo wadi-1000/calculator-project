e2e800d8ed1eb2f1ed8aa6c03b4102231358567a
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-06-09 15:45:53 +0300

    Added all folders

---
+++ javascript.js/scripts.js
@@ -0,0 +1,8 @@
+let add = function(number1, number2) {
+    return number1 + number2;
+    };
+    let number1 = parseInt(prompt("Enter a number:"));
+    let number2 = parseInt(prompt("Enter another number:"));
+    alert(add(number1, number2))
+    
+    
